376607713c3712d7591f5d56efebe09b1e48a01b
RobeZH <37002130+RobeZH@users.noreply.github.com>
2018-04-09 23:48:50 -0500

    Initialize

---
+++ src/Graph.java
@@ -0,0 +1,78 @@
+/**
+ * Undirected and unweighted graph implementation
+ * 
+ * @param <E> type of a vertex
+ * 
+ * @author sapan (sapan@cs.wisc.edu)
+ * 
+ */
+public class Graph<E> implements GraphADT<E> {
+    
+    /**
+     * Instance variables and constructors
+     */
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public E addVertex(E vertex) {
+        return vertex;
+        
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public E removeVertex(E vertex) {
+        return vertex;
+        
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean addEdge(E vertex1, E vertex2) {
+        return false;
+        
+    }    
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean removeEdge(E vertex1, E vertex2) {
+        return false;
+        
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean isAdjacent(E vertex1, E vertex2) {
+        return false;
+        
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Iterable<E> getNeighbors(E vertex) {
+        return null;
+        
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Iterable<E> getAllVertices() {
+        return null;
+        
+    }
+
+}
